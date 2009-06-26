3484f4bf0880986273d383f93f4e805b9ce4dc57
Erik L. Vonderscheer <evonder@gmail.com>
2009-06-26 06:45:17 +0000

    typo squash!

--- BloodSurge.lua
+++ BloodSurge.lua
@@ -4,8 +4,8 @@ local BS = BloodSurge
 
 local MAJOR_VERSION = "1.0"
 local MINOR_VERSION = 000 + tonumber(("$Revision: @project-revision@ $"):match("%d+"))
-TF3.version = MAJOR_VERSION .. "." .. MINOR_VERSION
-TF3.date = string.sub("$Date: @file-date-iso@ $", 8, 17)
+BS.version = MAJOR_VERSION .. "." .. MINOR_VERSION
+BS.date = string.sub("$Date: @file-date-iso@ $", 8, 17)
 
 --[[ Locals ]]--
 local find = _G.string.find

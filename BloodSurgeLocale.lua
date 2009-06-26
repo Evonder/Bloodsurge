c597bf4f017453bce9b8a4f67819f213702b4844
Erik L. Vonderscheer <evonder@gmail.com>
2009-06-26 06:33:48 +0000

    Copy and paste typo :P

--- BloodSurgeLocale.lua
+++ BloodSurgeLocale.lua
@@ -8,25 +8,25 @@ local debug = false
 debug = true
 --@end-debug@]===]
 
-local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "enUS", true, debug)
+local L =  LibStub("AceLocale-3.0"):NewLocale("BloodSurge", "enUS", true, debug)
 if L then
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "enUS" then return end
 end
 
-local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "deDE", true, debug)
+local L =  LibStub("AceLocale-3.0"):NewLocale("BloodSurge", "deDE", true, debug)
 if L then
 --@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "deDE" then return end
 end
 
-local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "zhCN", true, debug)
+local L =  LibStub("AceLocale-3.0"):NewLocale("BloodSurge", "zhCN", true, debug)
 if L then
 --@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "zhCN" then return end
 end
 
-local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "zhTW", true, debug)
+local L =  LibStub("AceLocale-3.0"):NewLocale("BloodSurge", "zhTW", true, debug)
 if L then
 --@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "zhTW" then return end
